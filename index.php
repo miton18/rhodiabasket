438ccf4b72cbe9c15a5fd1e11336c12bb3592810
miton <rcd18@hotmail.fr>
2014-09-01 22:57:39 +0200

    Signed-off-by: miton <rcd18@hotmail.fr>

--- index.php
+++ index.php
@@ -161,5 +161,219 @@
 				});
 			});
 		</script>
+		<script>
+	
+	var App = angular.module('App',[]);
+	// CONTROLEUR VIEW EQUIPE
+	App.controller('equipe', function($scope) 
+	{
+		$scope.Entraineur = [
+			{
+				"nom"   : "Tristan",
+				"poule" : "Cadets",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/face/tristan.jpg"
+			},
+			{
+				"nom"   : "Mélanie",
+				"poule" : "Minimes (filles)",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/face/melanie.jpg"
+			},
+			{
+				"nom"   : "Elsa",
+				"poule" : "Poussins",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/face/elsa.jpg"
+			},
+			{
+				"nom"   : "Valérie",
+				"poule" : "Poussins / Poussines",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Anthony",
+				"poule" : "Benjamins",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Charlotte",
+				"poule" : "Minimes (garçons)",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Coralie",
+				"poule" : "Minimes (garçons)",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/face/coralie.jpg"
+			},
+			{
+				"nom"   : "Cyrielle",
+				"poule" : "Benjamines",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Karine",
+				"poule" : "Cadets",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Loris",
+				"poule" : "Benjamines",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Malik",
+				"poule" : "Benjamins",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/global/personne.png"
+			},
+			{
+				"nom"   : "Nicolas",
+				"poule" : "Minimes (filles)",
+				"mail"  : "---",
+				"role"  : "Entraineur / Resp. technique",
+				"photo" : "img/face/nicolas.jpg"
+			},
+			{
+				"nom"   : "Ousmane",
+				"poule" : "Seniors (filles)",
+				"mail"  : "---",
+				"role"  : "Entraineur",
+				"photo" : "img/face/ousmane.jpg"
+			}
+			/*{
+				"nom"   : "Rémi Collignon",
+				"poule" : "http://remi.rcdinfo.fr",
+				"mail"  : "contact@rcdinfo.fr",
+				"role"  : "Developpeur web",
+				"photo" : "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfp1/t1.0-1/c27.0.160.160/p160x160/10308337_10202773982822199_7573237223106598054_n.jpg"
+			}*/
+		];
+	});
+	// CONTROLEUR VIEW CONTACT
+	App.controller('mapi', function($scope)
+	{
+
+		$scope.gymnases = [
+			{
+				"nom": "Gymnase Frédéric MISTRAL",
+				"adr": "rue Victor Renelle 38550 St Maurice l’Exil",
+				"lat": "45.396264",
+				"lon": "4.780496"
+			},
+			{
+				"nom": "Gymnase Bacquet",
+				"adr": "avenue Beyle Stendhal 38150 Roussillon",
+				"lat": "45.365006",
+				"lon": "4.806152"
+			},
+			{
+				"nom": "Gymnase Joseph Plat",
+				"adr": "impasse Jean Bouin 38150 Salaise sur Sanne",
+				"lat": "45.342242",
+				"lon": "4.815425"
+			},
+			{
+				"nom": "Gymnase Pierre Quinon",
+				"adr": "rue Edouard Aubert 38150 Salaise sur Sanne.",
+				"lat": "45.339812",
+				"lon": "4.819527"
+			}
+		];
+		function initialize() 
+	    {
+	        var mapOptions = 
+	        {
+	          center: new google.maps.LatLng(45.378040, 4.811272), 
+	          zoom: 12,
+	          mapTypeId: google.maps.MapTypeId.HYBRID
+	        };
+	        var map = new google.maps.Map( document.getElementById("map-canvas"), mapOptions);
+
+	        //pointeurs gymnases
+	        /*
+	        Gymnase Frédéric MISTRAL, rue Victor Renelle 38550 St Maurice l’Exil
+			Gymnase Bacquet, avenue Beyle Stendhal 38150 Roussillon
+			Gymnase Joseph Plat, impasse Jean Bouin 38150 Salaise sur Sanne
+			Gymnase Pierre Quinon, rue Edouard Aubert 38150 Salaise sur Sanne.
+	        */
+	        /*var marker1 = new google.maps.Marker(
+	        {
+	      		position: new google.maps.LatLng(45.396264, 4.780496),
+	      		map: map,
+	      		title: 'Gymnase Frédéric MISTRAL'
+	  		});
+	  		google.maps.event.addListener(marker1, 'click', function() 
+		    {
+		    	new google.maps.InfoWindow(
+		    	{
+		    		content: '<h5>' + marker1.title + '</h5>'
+		  		}).open(map,marker1);
+		  	});	
+
+	        var marker2 = new google.maps.Marker(
+	        {
+	      		position: new google.maps.LatLng(45.365006, 4.806152),
+	      		map: map,
+	      		title: 'Gymnase BACKET'
+	  		});
+	  		google.maps.event.addListener(marker2, 'click', function() 
+		    {
+		    	new google.maps.InfoWindow(
+		    	{
+		    		content: '<h5>' + marker2.title + '</h5>'
+		  		}).open(map,marker2);
+		  	});	
+
+	        var marker3 = new google.maps.Marker(
+	        {
+	      		position: new google.maps.LatLng(45.342242, 4.815425),
+	      		map: map,
+	      		title: 'Gymnase Joseph PLAT'
+	  		});
+	  		google.maps.event.addListener(marker3, 'click', function() 
+		    {
+		    	new google.maps.InfoWindow(
+		    	{
+		    		content: '<h5>' + marker3.title + '</h5>'
+		  		}).open(map,marker3);
+		  	});	
+
+	        var marker4 = new google.maps.Marker(
+	        {
+	      		position: new google.maps.LatLng(45.339812, 4.819527),
+	      		map: map,
+	      		title: 'Gymnase Pierre QUINON'
+	  		});
+	  		google.maps.event.addListener(marker4, 'click', function() 
+		    {
+		    	new google.maps.InfoWindow(
+		    	{
+		    		content: '<h5>' + marker4.title + '</h5>'
+		  		}).open(map,marker4);
+		  	});	*/
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+	});
+
+</script>
 	</body>
 </html>
