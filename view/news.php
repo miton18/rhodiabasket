cef3b78f164fd677a60734e104501471f91291b5
miton <rem.coll18@gmail.com>
2015-04-06 19:07:01 +0200

    add affiche2

--- view/news.php
+++ view/news.php
@@ -1,4 +1,39 @@
 <div>
+	<!--h2>[BIENTOT] Paëla</h2>
+	<a href="img/affiche2.pdf"><iframe class="col-lg-12" style="height: 780px" src="img/affiche2.pdf" ></iframe></a>
+
 	<h2>[PASSE] Matinée Diots</h2>
-	<iframe class="col-lg-12" style="height: 780px" src="img/affiche1.pdf" ></iframe>
-</div>
+	<iframe class="col-lg-12" style="height: 780px" src="img/affiche1.pdf" ></iframe-->
+
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="false">
+  		<div class="panel panel-default">
+    		<div class="panel-heading" role="tab" id="headingOne">
+      			<h4 class="panel-title">
+        			<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">[BIENTOT] Paëla</a>
+      			</h4>
+    		</div>
+    		<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      			<div class="panel-body">
+      				<a href="img/affiche2.pdf">
+      					<iframe class="col-lg-12" style="height: 780px" src="img/affiche2.pdf" ></iframe>
+      				</a>
+      			</div>
+    		</div>
+  		</div>
+
+
+  		<div class="panel panel-default">
+    		<div class="panel-heading" role="tab">
+      			<h4 class="panel-title">
+        			<a data-toggle="collapse" data-parent="#accordion" href="#collapsetwo" aria-expanded="false" aria-controls="collapsetwo">[PASSE] Matinée Diots</a>
+      			</h4>
+    		</div>
+    		<div id="collapsetwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="collapsetwo">
+      			<div class="panel-body">
+      				<a href="img/affiche1.pdf">
+      					<iframe class="col-lg-12" style="height: 780px" src="img/affiche1.pdf" ></iframe>
+      				</a>
+      			</div>
+    		</div>
+  		</div>
+	</div>
